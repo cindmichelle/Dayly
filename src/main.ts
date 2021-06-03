4d5f1bd5c8a7330acd64eee044d693f2c5c61b6b
Cindy Michelle <cindy@kodefox.com>
2021-06-04 02:23:01 +0700

    wip

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,17 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+// app.use(express.urlencoded({extended:true})
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  let { name, age } = req.query;
+
+  res.json({ data: `Hello ${name} sayang ${age}` });
+});
+
+app.post('/register', (req, res) => {
+  console.log('>>', req.body);
 });
 
 app.listen(port, () => {
